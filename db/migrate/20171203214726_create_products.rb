34ddce15b374d0cb646beea61852ba90f5a12535
Javier Jimenez <javierrcc522@gmail.com>
2017-12-03 14:02:22 -0800

    migrates are ready to roll

--- db/migrate/20171203214726_create_products.rb
+++ db/migrate/20171203214726_create_products.rb
@@ -1,6 +1,10 @@
 class CreateProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :products do |t|
+      t.column :name, :string
+      t.column :price, :decimal
+
+      t.timestamps
     end
   end
 end
--- db/migrate/20171203214803_create_order_items.rb
+++ db/migrate/20171203214803_create_order_items.rb
@@ -1,6 +1,9 @@
 class CreateOrderItems < ActiveRecord::Migration[5.1]
   def change
     create_table :order_items do |t|
+      t.column :quantify, :integer
+      t.column :product_id, :integer
+      t.column :order_id, :integer
     end
   end
 end
